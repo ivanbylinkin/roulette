38763ab8445034f557064df72a275f317a0a6517
Ivan Bylinkin <ivanbylinkin@gmail.com>
2015-10-13 02:32:06 -0400

    defined the rouletteGen function which generates a number between 0 and 35

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 //  RouletteWheel
 
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+// define forward declarations below
+int rouletteGen();
 
 int main() {
+    // set up a random seed
+    srand(time(NULL));
     
     return 0;
+}
+
+int rouletteGen(){
+    // returns a random number between 0 and 35
+    return rand() % 36;
 }
